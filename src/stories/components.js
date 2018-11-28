ea4ba0c7fd04e3fe7612527827dad57aa10d2016
AMin <alexanderocean16@gmail.com>
2018-11-28 09:56:13 +0800

    footer建置

--- src/stories/components.js
+++ src/stories/components.js
@@ -2,5 +2,8 @@ import React from 'react'
 
 import { storiesOf } from '@storybook/react'
 import { Nav } from '../components/nav/desktop'
+import { Footer } from '../components/footer/footer'
 
-storiesOf('Components', module).add('Nav', () => <Nav />)
+storiesOf('Components', module)
+         .add('Nav', () => <Nav />)
+         .add('Footer', () => <Footer />)
