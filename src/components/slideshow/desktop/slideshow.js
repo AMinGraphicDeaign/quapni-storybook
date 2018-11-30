f3e3cf261c2cf95383362359fbe499c2ce5edb03
AMin <alexanderocean16@gmail.com>
2018-11-30 14:16:44 +0800

    slideshow 改寫成 react版本

--- src/components/slideshow/desktop/slideshow.js
+++ src/components/slideshow/desktop/slideshow.js
@@ -1,50 +1,61 @@
-import React from 'react'
+import React, { Component } from "react";
+import Slider from "react-slick";
+import '../../../../node_modules/slick-carousel/slick/slick.css'
+import '../../../../node_modules/slick-carousel/slick/slick-theme.css'
 import './slideshow.css'
 
 
-export const Slideshow = () => (
-    <div className="box-margin">
-        <div className="carousel-wrapper">
-            <span id="item-1" />
-            <span id="item-2" />
-            <span id="item-3" />
-            <span id="item-4" />
-            <div className="carousel-item item-1">
-                <div className="slogan">
-                    <h1 style={{ color: "#ffffff" }}>我們相信</h1>
-                    <h1 style={{ color: "#ffffff" }}>唯有用心堅持</h1>
-                    <h1 style={{ color: "#ffffff" }}>才能看見與眾不同</h1>
-                </div>
-                <a className="arrow arrow-prev" href="#item-4" />
-                <a className="arrow arrow-next" href="#item-2" />
+export class Slideshow extends Component {
+    render() {
+        const settings = {
+            dots: true,
+            fade: true,
+            arrows: true,
+            infinite: true,
+            speed: 600,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            //autoplay: true,
+            //autoplaySpeed: 2000,
+        };
+        return (
+            <div>
+                <Slider {...settings} className="slick-wrapper">
+                    <div className="slick-img-mask">
+                        <img src={"https://i.imgur.com/MUMi2mK.jpg"} />
+                        <div className="slogan">
+                            我們相信<br />唯有用心堅持<br />才能看見與眾不同
+                        </div>
+                    </div>
+
+                    <div className="slick-img-mask">
+                        <img src={"https://i.imgur.com/zKrjV3W.jpg"} />
+                        <div className="slick-title">前打輪</div>
+                        <div className="paragraph-style">
+                            <div>單款風格化。固定柄設計</div><br />
+                            <div style={{ color: "#ff7576" }}>高品質輪座 堅固耐用</div>
+                        </div>
+                    </div>
+
+                    <div className="slick-img-mask">
+                        <img src={"https://i.imgur.com/x9pvoaA.jpg"} />
+                        <div className="slick-title">納茲卡</div>
+                        <div className="paragraph-notestyle">
+                            <div>靈活無限。順暢無阻</div><br />
+                            <div style={{ color: "#ff7576", fontSize: "3vmin" }}><b>日本第一品牌 NSK 培林</b></div>
+                        </div>
+                    </div>
+
+                    <div className="slick-img-mask">
+                        <img src={"https://i.imgur.com/FOMtxiQ.jpg"} />
+                        <div className="slick-title">山茶</div>
+                        <div className="paragraph-style">
+                            <div>專利設計 - 壓鈕機構快拆</div><br />
+                            <div style={{ color: "#ff7576" }}><b>1秒快速拆卸</b></div>
+                        </div>
+                    </div>
+                </Slider>
             </div>
-            <div className="carousel-item item-2">
-                <div><b>前打輪</b></div><br />
-                <div className="paragraph-style">
-                    <div>單款風格化。固定柄設計</div><br />
-                    <div style={{ color: "#ff7576" }}>高品質輪座 堅固耐用</div>
-                </div>
-                <a className="arrow arrow-prev" href="#item-1" />
-                <a className="arrow arrow-next" href="#item-3" />
-            </div>
-            <div className="carousel-item item-3">
-                <div><b>山茶</b></div><br />
-                <div className="paragraph-style">
-                    <div>專利設計 - 壓鈕機構快拆</div><br />
-                    <div style={{ color: "#ff7576" }}><b>1秒快速拆卸</b></div>
-                </div>
-                <a className="arrow arrow-prev" href="#item-2" />
-                <a className="arrow arrow-next" href="#item-4" />
-            </div>
-            <div className="carousel-item item-4">
-                <div><b>納茲卡</b></div><br />
-                <div className="paragraph-notestyle">
-                    <div>靈活無限。順暢無阻</div><br />
-                    <div style={{ color: "#ff7576",fontSize:"3vmin" }}><b>日本第一品牌 NSK 培林</b></div>
-                </div>
-                <a className="arrow arrow-prev" href="#item-3" />
-                <a className="arrow arrow-next" href="#item-1" />
-            </div>
-        </div>
-    </div>
-)
+        );
+    }
+}
