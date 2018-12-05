be8609b94a5962791b21ea9299a3d58854ce3210
AMin <alexanderocean16@gmail.com>
2018-12-05 09:16:21 +0800

    slideshow 手機版頁面 css 命名整理

--- src/components/slideshow/mobile/slideshow.js
+++ src/components/slideshow/mobile/slideshow.js
@@ -4,10 +4,10 @@ import { quapni_logo } from '../../../images/logo';
 import { nazaca_02, nazaca_01, camellia_01 } from '../../../images/slideshow';
 
 export const MobileSlideshow = () => (
-    // <Responsive maxWidth={767}>
+    // <Responsive maxWidth={767}> 
     <div>
-        <div className="text-box">
-            <div className="mobile-slogan">
+        <div className="slideshow-text-box">
+            <div className="slideshow-slogan">
                 我們相信<br />
                 唯有用心堅持<br />
                 才能看見與眾不同。<br />
@@ -16,34 +16,34 @@ export const MobileSlideshow = () => (
 
         <div>
             <img alt="" src={nazaca_02} />
-            <div className="text-box">
-                <div className="product-intro">
-                    <div className="product-intro-title">前打輪</div>
+            <div className="slideshow-text-box">
+                <div className="slideshow-intro">
+                    <div className="slideshow-intro-title">前打輪</div>
                     單款風格化。固定柄設計<br />
                     高品質輪座 堅固耐用
                 </div>
             </div>
 
             <img alt="" src={nazaca_01} />
-            <div className="text-box">
-                <div className="product-intro">
-                    <div className="product-intro-title">納茲卡</div>
+            <div className="slideshow-text-box">
+                <div className="slideshow-intro">
+                    <div className="slideshow-intro-title">納茲卡</div>
                     靈活無限。順暢無阻<br />
                     <b>日本第一品牌 NSK 培林</b>
                 </div>
             </div>
 
             <img alt="" src={camellia_01} />
-            <div className="text-box">
-                <div className="product-intro">
-                    <div className="product-intro-title">山茶</div>
+            <div className="slideshow-text-box">
+                <div className="slideshow-intro">
+                    <div className="slideshow-intro-title">山茶</div>
                     專利設計 - 壓鈕機構快拆<br />
                     <b>1秒快速拆卸</b>
                 </div>
             </div>
 
-            <div className="text-box">
-                <div className="logo-style"><img alt="" src={quapni_logo} /></div>
+            <div className="slideshow-text-box">
+                <div className="slideshow-logo"><img alt="" src={quapni_logo} /></div>
             </div>
 
         </div>
